f81bb1f4edd32b98eae440173aac540d28149f82
Sami6720 <78088136+Sami6720@users.noreply.github.com>
2024-06-23 17:53:16 -0400

    Add sourcing of .bashrc file to the end of all setup aliases

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ if __name__ == '__main__':
         os.environ['HOME'], '.bashrc')
     with open(bashrc_path, 'a') as file:
         file.write('\n#HARP SET UP STUFF\n')
-        file.write("alias s='python  ~/quick_navigate/main.py -s'\n")
-        file.write("alias a='python  ~/quick_navigate/main.py -a'\n")
-        file.write("alias r='python  ~/quick_navigate/main.py -r'\n")
-        file.write("alias c='python  ~/quick_navigate/main.py -c'\n")
+        file.write("alias s='python  ~/quick_navigate/main.py -s; source ~/.bashrc'\n")
+        file.write("alias a='python  ~/quick_navigate/main.py -a; source ~/.bashrc'\n")
+        file.write("alias r='python  ~/quick_navigate/main.py -r; source ~/.bashrc'\n")
+        file.write("alias c='python  ~/quick_navigate/main.py -c; source ~/.bashrc'\n")
         file.write('#HARP SET UP STUFF\n')
