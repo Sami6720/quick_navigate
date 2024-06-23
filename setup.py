959d12225c155c506d20089996bea0b75efcd749
Sami6720 <78088136+Sami6720@users.noreply.github.com>
2024-06-23 17:14:38 -0400

    Fix path where quick_nav is located
    Same as last

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ import os
 
 if __name__ == '__main__':
     bashrc_path = os.path.join(
-        os.environ['HOMEDRIVE'] + os.environ['HOMEPATH'], '.bashrc')
+        os.environ['HOME'], '.bashrc')
     with open(bashrc_path, 'a') as file:
         file.write('\n#HARP SET UP STUFF\n')
-        file.write("alias s='python -m quick_navigate.main -s'\n")
-        file.write("alias a='python -m quick_navigate.main -a'\n")
-        file.write("alias r='python -m quick_navigate.main -r'\n")
-        file.write("alias c='python -m quick_navigate.main -c'\n")
+        file.write("alias s='python -m ~/quick_navigate.main -s'\n")
+        file.write("alias a='python -m ~/quick_navigate.main -a'\n")
+        file.write("alias r='python -m ~/quick_navigate.main -r'\n")
+        file.write("alias c='python -m ~/quick_navigate.main -c'\n")
         file.write('#HARP SET UP STUFF\n')
