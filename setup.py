d573cb8aa8e7a9a45e5b879c050ce792a30e0d66
Sami6720 <78088136+Sami6720@users.noreply.github.com>
2024-06-25 22:31:36 -0400

    Fix wrong file path

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ if __name__ == '__main__':
     file_path_base = os.path.join(
         os.environ['HOME'], '.qn')
     file_path = os.path.join(file_path_base, 'config.sh')
-    os.makedirs(file_path, exist_ok=True)
-    with open(file_path_base, 'a') as file:
+    os.makedirs(file_path_base, exist_ok=True)
+    with open(file_path, 'a') as file:
         file.write("alias s='python  ~/quick_navigate/main.py -s; source ~/.qn/curr_aliases.sh\n")
         file.write("alias a='python  ~/quick_navigate/main.py -a; source ~/.qn/curr_aliases.sh'\n")
         file.write("alias r='python  ~/quick_navigate/main.py -r; source ~/.qn/curr_aliases.sh'\n")
