0295d286bf464bbac2c84716cd46a21195bfbbf4
Sami6720 <78088136+Sami6720@users.noreply.github.com>
2024-06-25 22:34:19 -0400

    Fix missing quote mark

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     file_path = os.path.join(file_path_base, 'config.sh')
     os.makedirs(file_path_base, exist_ok=True)
     with open(file_path, 'a') as file:
-        file.write("alias s='python  ~/quick_navigate/main.py -s; source ~/.qn/curr_aliases.sh\n")
+        file.write("alias s='python  ~/quick_navigate/main.py -s; source ~/.qn/curr_aliases.sh'n")
         file.write("alias a='python  ~/quick_navigate/main.py -a; source ~/.qn/curr_aliases.sh'\n")
         file.write("alias r='python  ~/quick_navigate/main.py -r; source ~/.qn/curr_aliases.sh'\n")
         file.write("alias c='python  ~/quick_navigate/main.py -c; source ~/.qn/curr_aliases.sh'\n")
