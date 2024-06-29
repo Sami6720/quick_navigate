8f64ead77e1862dc56e98f7f14f7217a7c8a050d
Sami6720 <78088136+Sami6720@users.noreply.github.com>
2024-06-28 22:35:16 -0400

    Update setup.py after refactoring

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ if __name__ == '__main__':
     file_path = os.path.join(file_path_base, 'config.sh')
     os.makedirs(file_path_base, exist_ok=True)
     with open(file_path, 'a') as file:
-        file.write("alias s='python  ~/quick_navigate/main.py -s; source ~/.qn/curr_aliases.sh'\n")
-        file.write("alias a='python  ~/quick_navigate/main.py -a; source ~/.qn/curr_aliases.sh'\n")
-        file.write("alias r='python  ~/quick_navigate/main.py -r; source ~/.qn/curr_aliases.sh'\n")
-        file.write("alias c='python  ~/quick_navigate/main.py -c; source ~/.qn/curr_aliases.sh'\n")
+        file.write("alias s='python  ~/quick_navigate/main.py --name show; source ~/.qn/curr_aliases.sh'\n")
+        file.write("alias a='python  ~/quick_navigate/main.py --name add; source ~/.qn/curr_aliases.sh'\n")
+        file.write("alias r='python  ~/quick_navigate/main.py --name remove; source ~/.qn/curr_aliases.sh'\n")
+        file.write("alias c='python  ~/quick_navigate/main.py --name cwd; source ~/.qn/curr_aliases.sh'\n")
+        file.write("alias u='python  ~/quick_navigate/main.py --name udpate; source ~/.qn/curr_aliases.sh'\n")
